710d2077795f16674e3da69ffe15459d57b6ecc6
jose_ademar <joseademarti@gmail.com>
2022-03-20 14:39:41 -0300

    First commit Project Creation

---
+++ src/test/java/carrental/web/project/NativeCarRentalWebProjectIT.java
@@ -0,0 +1,9 @@
+package carrental.web.project;
+
+import io.quarkus.test.junit.NativeImageTest;
+
+@NativeImageTest
+public class NativeCarRentalWebProjectIT extends CarRentalWebProjectTest {
+
+    // Execute the same tests but in native mode.
+}
