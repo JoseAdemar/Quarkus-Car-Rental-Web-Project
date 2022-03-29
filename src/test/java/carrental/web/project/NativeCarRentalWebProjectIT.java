376746e4d2ca42080baa2b3daee1b23529113af5
Andrea Bevilacqua <andrey87@alice.it>
2022-03-30 01:01:55 +0200

    Added Flyway configuration

--- src/test/java/carrental/web/project/NativeCarRentalWebProjectIT.java
+++
@@ -1,9 +0,0 @@
-package carrental.web.project;
-
-import io.quarkus.test.junit.NativeImageTest;
-
-@NativeImageTest
-public class NativeCarRentalWebProjectIT extends CarRentalWebProjectTest {
-
-    // Execute the same tests but in native mode.
-}
